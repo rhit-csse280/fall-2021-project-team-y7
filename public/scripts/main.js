76b5655b363ba6b1d8afd157301caf25502f518d
rhit-boatmaee <boatmaee@gmail.com>
2021-10-25 10:02:49 -0400

    add task changes

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,20 +16,34 @@ function htmlToElement(html) {
 rhit.ListPageController = class {
 	constructor() {
 
+		if(document.querySelector("#addTaskButton") != null){
 		document.querySelector("#addTaskButton").addEventListener("click", (event) => {
 
 
 			document.location.href = "./addTask.html"
-			const list = document.querySelector("#cardsContainer");
-			const card = htmlToElement(`<div class = "card"> 
-		<div class = "card-body"> 
-		<h5 class = "card-title"> Default Task </h5> 
-		<h6 class = "card-subtitle" mb-2 text-muted">Default Date</h6> 
-		</div> </div>`);
-			list.appendChild(card);
+
+			
+		// 	const list = document.querySelector("#cardsContainer");
+		// 	const card = htmlToElement(`<div class = "card"> 
+		// <div class = "card-body"> 
+		// <h5 class = "card-title"> Default Task </h5> 
+		// <h6 class = "card-subtitle" mb-2 text-muted">Default Date</h6> 
+		// </div> </div>`);
+		// 	list.appendChild(card);
 
 		});
-
+	}
+		if(document.querySelector("#addButton") != null){
+		document.querySelector("#addButton").addEventListener("click", (event) => {
+			const name = document.querySelector("#taskName").value;
+			const date = document.querySelector("#dueDate").value;
+			const desc = document.querySelector("#desc").value;
+			
+			rhit.fbTasksManager.add(name, date, desc);
+			document.location.href = "index.html";
+		});
+	}	
+	if(document.querySelector("#startButton") != null){
 		document.querySelector("#startButton").addEventListener("click", (event) => {
 
 			timerOn = true;
@@ -41,6 +55,8 @@ rhit.ListPageController = class {
 
 
 		});
+	}
+	if(document.querySelector("#stopButton") != null){
 		document.querySelector("#stopButton").addEventListener("click", (event) => {
 			timerOn = false;
 			rhit.stopTimer();
@@ -55,8 +71,9 @@ rhit.ListPageController = class {
 
 
 		});
+	}
 
-
+if(document.querySelector("#pauseButton") != null){
 		document.querySelector("#pauseButton").addEventListener("click", (event) => {
 			timerOn = false;
 			rhit.pauseTimer();
@@ -69,10 +86,96 @@ rhit.ListPageController = class {
 			document.querySelector("#pauseButton").style.display = "none";
 
 		});
+	}
+
+		rhit.fbTasksManager.beginListening(this.updateList.bind(this));
+	}
+	_createCard(task) {
+		return htmlToElement(`<div class="card">
+		<div class="card-body">
+		  <input type="checkbox">
+		  <h5 class="card-title"> ${task.name}</h5>
+		  <h6 class="card-subtitle mb2 ">${task.date}</h6>
+		</div>
+	  </div>`);
+	}
+	updateList() {
+		
+		const newList = htmlToElement('<div id = "cardsContainer"></div>');
+
+		for (let i = 0; i < rhit.fbTasksManager.length; i++) {
+			const task = rhit.fbTasksManager.getTaskAtIndex(i);
+			const newCard = this._createCard(task);
 
 
+			newCard.onclick = (event) => {
+				
+			//	window.location.href = `/task.html?id=${task.id}`;
 
+			}
+			newList.appendChild(newCard);
 
+		}
+	
+		const oldList = document.querySelector("#cardsContainer");
+		oldList.removeAttribute("id");
+		oldList.hidden = true;
+	
+		oldList.parentElement.appendChild(newList);
+
+	}
+}
+rhit.FbTasksManager = class {
+	constructor() {
+		console.log("created tasks manager");
+		this._documentSnapshots = [];
+		this._ref = firebase.firestore().collection("Tasks");
+		this._unsubscribe = null;
+	}
+	add(name, date, desc) {
+		console.log("adding a task");
+		this._ref.add({
+			//trouble with adding to firebase
+				["Name"]: name,
+				["Due Date"]: date,
+				["Date Created"]:firebase.firestore.Timestamp.now(),
+				["Description"]: desc,
+			})
+			.then(function (docRef) {
+				console.log("Document written with ID: ", docRef.id);
+			})
+			.catch(function (error) {
+				console.error("Error adding document: ", error);
+			})
+
+	}
+	beginListening(changeListener) {
+		this._unsubscribe = this._ref.orderBy("Date Created", "desc").limit(50).onSnapshot((querySnapshot) => {
+			this._documentSnapshots = querySnapshot.docs;
+			changeListener();
+
+		});
+	}
+	stopListening() {
+		this._unsubscribe();
+	}
+	
+	get length() {
+		return this._documentSnapshots.length;
+	}
+	getTaskAtIndex(index) {
+		const docSnapshot = this._documentSnapshots[index];
+		const task = new rhit.Task(docSnapshot.id,
+			docSnapshot.get("Name"),
+			docSnapshot.get("Due Date"));
+		return task;
+	}
+}
+rhit.Task= class {
+	constructor(id, name, date) {
+		this.id = id;
+		this.name = name;
+		this.date = date;
 	}
 }
 rhit.stopTimer = function () {
@@ -148,6 +251,10 @@ rhit.startTimer = function () {
 
 rhit.main = function () {
 	console.log("Ready");
+	if (document.querySelector("#mainPage")) {
+		rhit.fbTasksManager = new rhit.FbTasksManager();
+		new rhit.ListPageController();
+	}
 	new rhit.ListPageController();
 	$("#evoCalendar").evoCalendar({
 		todayHighlight: true,
